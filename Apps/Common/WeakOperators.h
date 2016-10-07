108123761ca586bd4edcc5184d7dd3b8c5b527b4
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2016-10-07 12:20:26 +0200

    changed: make WeakOperators a class with static functions
    allows for templating over operator classes

--- Apps/Common/WeakOperators.h
+++ Apps/Common/WeakOperators.h
@@ -18,16 +18,17 @@ class Vec3;
 #include "FiniteElement.h"
 #include "MatVec.h"
 
-namespace WeakOperators
+class WeakOperators
 {
+public:
   //! \brief Compute an advection term.
   //! \param[out] EM The element matrix to add contribution to
   //! \param[in] fe The finite element to evaluate for
   //! \param[in] AC Advecting field
   //! \param[in] scale Scaling factor for contribution
   //! \param[in] basis Basis to use
-  void Advection(Matrix& EM, const FiniteElement& fe,
-                 const Vec3& AC, double scale=1.0, int basis=1);
+  static void Advection(Matrix& EM, const FiniteElement& fe,
+                        const Vec3& AC, double scale=1.0, int basis=1);
 
   //! \brief Compute a (nonlinear) convection term.
   //! \param[out] EM The element matrix to add contribution to
@@ -36,9 +37,9 @@ namespace WeakOperators
   //! \param[in] conservative True to use the conservative formulation
   //! \param[in] basis Basis to use
   template<class T>
-  void Convection(Matrix& EM, const FiniteElement& fe,
-                  const Vec3& U, const T& dUdX, double scale,
-                  bool conservative=false, int basis=1)
+  static void Convection(Matrix& EM, const FiniteElement& fe,
+                         const Vec3& U, const T& dUdX, double scale,
+                         bool conservative=false, int basis=1)
   {
     size_t cmp = EM.rows() / fe.basis(basis).size();
     if (conservative) {
@@ -69,8 +70,8 @@ namespace WeakOperators
   //! \param[in] scale Scaling factor for contribution
   //! \param[in] basis Basis for field
   //! \param[in] tbasis Test function basis
-  void Divergence(Matrix& EM, const FiniteElement& fe,
-                  double scale=1.0, int basis=1, int tbasis=1);
+  static void Divergence(Matrix& EM, const FiniteElement& fe,
+                         double scale=1.0, int basis=1, int tbasis=1);
 
   //! \brief Compute a divergence term.
   //! \param[out] EV The element vector to add contribution to
@@ -78,8 +79,8 @@ namespace WeakOperators
   //! \param[in] D Divergence of field
   //! \param[in] scale Scaling factor for contribution
   //! \param[in] basis Test function basis
-  void Divergence(Vector& EV, const FiniteElement& fe,
-                  const Vec3& D, double scale=1.0, int basis=1);
+  static void Divergence(Vector& EV, const FiniteElement& fe,
+                         const Vec3& D, double scale=1.0, int basis=1);
 
   //! \brief Compute a gradient term for a (potentially mixed) vector/scalar field.
   //! \param[out] EM The element matrix to add contribution to
@@ -87,16 +88,16 @@ namespace WeakOperators
   //! \param[in] scale Scaling factor for contribution
   //! \param[in] basis Basis for field
   //! \param[in] tbasis Test function basis
-  void Gradient(Matrix& EM, const FiniteElement& fe,
-                double scale=1.0, int basis=1, int tbasis=1);
+  static void Gradient(Matrix& EM, const FiniteElement& fe,
+                       double scale=1.0, int basis=1, int tbasis=1);
 
   //! \brief Compute a gradient term.
   //! \param[out] EV The element vector to add contribution to
   //! \param[in] fe The finite element to evaluate for
   //! \param[in] scale Scaling factor for contribution
   //! \param[in] tbasis Test function basis
-  void Gradient(Vector& EV, const FiniteElement& fe,
-                double scale=1.0, int basis=1);
+  static void Gradient(Vector& EV, const FiniteElement& fe,
+                       double scale=1.0, int basis=1);
 
   //! \brief Compute a laplacian.
   //! \param[out] EM The element matrix to add contribution to
@@ -104,24 +105,24 @@ namespace WeakOperators
   //! \param[in] scale Scaling factor for contribution
   //! \param[in] stress Whether to add extra stress formulation terms
   //! \param[in] basis Basis to use
-  void Laplacian(Matrix& EM, const FiniteElement& fe,
-                 double scale=1.0, bool stress=false, int basis=1);
+  static void Laplacian(Matrix& EM, const FiniteElement& fe,
+                        double scale=1.0, bool stress=false, int basis=1);
 
   //! \brief Compute a heteregenous coefficient laplacian.
   //! \param[out] EM The element matrix to add contribution to
   //! \param[out] K The coefficient matrix
   //! \param[in] fe The finite element to evaluate for
   //! \param[in] scale Scaling factor for contribution
-  void LaplacianCoeff(Matrix& EM, const Matrix& K, const FiniteElement& fe,
-                      double scale=1.0, int basis=1);
+  static void LaplacianCoeff(Matrix& EM, const Matrix& K, const FiniteElement& fe,
+                             double scale=1.0, int basis=1);
 
   //! \brief Compute a mass term.
   //! \param[out] EM The element matrix to add contribution to
   //! \param[in] fe The finite element to evaluate for
   //! \param[in] scale Scaling factor for contribution
   //! \param[in] basis Basis to use
-  void Mass(Matrix& EM, const FiniteElement& fe,
-            double scale=1.0, int basis=1);
+  static void Mass(Matrix& EM, const FiniteElement& fe,
+                   double scale=1.0, int basis=1);
 
   //! \brief Compute a source term.
   //! \param[out] EV The element vector to add contribution to
@@ -129,8 +130,8 @@ namespace WeakOperators
   //! \param[in] scale Scaling factor for contribution
   //! \param[in] basis Basis to use
   //! \param[in] cmp Component to add (0 for all)
-  void Source(Vector& EV, const FiniteElement& fe,
-              double scale=1.0, int cmp=1, int basis=1);
+  static void Source(Vector& EV, const FiniteElement& fe,
+                     double scale=1.0, int cmp=1, int basis=1);
 
   //! \brief Compute a vector-source term.
   //! \param[out] EV The element vector to add contribution to
@@ -138,8 +139,8 @@ namespace WeakOperators
   //! \param[in] scale Vector with contributions
   //! \param[in] scale Scaling factor for contribution
   //! \param[in] basis Basis to use
-  void Source(Vector& EV, const FiniteElement& fe,
-              const Vec3& f, double scale=1.0, int basis=1);
-}
+  static void Source(Vector& EV, const FiniteElement& fe,
+                     const Vec3& f, double scale=1.0, int basis=1);
+};
 
 #endif
