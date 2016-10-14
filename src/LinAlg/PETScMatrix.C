bdb5df2b7a33f9d3ab8e1049ff8cc9f91dfdad5a
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2016-10-14 10:18:22 +0200

    s

--- src/LinAlg/PETScMatrix.C
+++ src/LinAlg/PETScMatrix.C
@@ -512,29 +512,20 @@ bool PETScMatrix::solve (const SystemVector& b, SystemVector& x, bool newLHS)
 
 bool PETScMatrix::solve (const Vec& b, Vec& x, bool newLHS, bool knoll)
 {
-  // Reset linear solver
-  if (nLinSolves && solParams.getIntValue("reset_solves"))
-    if (nLinSolves%solParams.getIntValue("reset_solves") == 0) {
-      KSPDestroy(&ksp);
-      KSPCreate(*adm.getCommunicator(),&ksp);
-      setParams = true;
-    }
-
-  Mat P;
-  if (mxv) {
-    const PetscInt neq  = adm.dd.getMaxEq()- adm.dd.getMinEq() + 1;
-    MatCreateShell(*adm.getCommunicator(), neq, neq,
-                   PETSC_DETERMINE, PETSC_DETERMINE, mxv, &A);
-    MatShellSetOperation(A, MATOP_MULT, (void(*)(void))&SIMMxV);
-//    static MatNullSpace meh;
-//    MatNullSpaceCreate(*adm.getCommunicator(),PETSC_TRUE,0,nullptr,&meh);
-//    MatSetNullSpace(A, meh);
-    MatSetUp(A);
-    mxv->evalPC(P);
-
-  }
-
   if (setParams) {
+    Mat P;
+    if (mxv) {
+      const PetscInt neq  = adm.dd.getMaxEq()- adm.dd.getMinEq() + 1;
+      MatCreateShell(*adm.getCommunicator(), neq, neq,
+                     PETSC_DETERMINE, PETSC_DETERMINE, mxv, &A);
+      MatShellSetOperation(A, MATOP_MULT, (void(*)(void))&SIMMxV);
+      static MatNullSpace meh;
+      MatNullSpaceCreate(*adm.getCommunicator(),PETSC_TRUE,0,nullptr,&meh);
+      MatSetNullSpace(A, meh);
+      MatSetUp(A);
+      mxv->evalPC(P);
+      MatSetNullSpace(P, meh);
+    }
 #if PETSC_VERSION_MINOR < 5
     KSPSetOperators(ksp,A,mxv ? P : A, newLHS ? SAME_NONZERO_PATTERN : SAME_PRECONDITIONER);
 #else
@@ -545,7 +536,7 @@ bool PETScMatrix::solve (const Vec& b, Vec& x, bool newLHS, bool knoll)
       return false;
     setParams = false;
   }
-  mxv = nullptr;
+
   if (knoll)
     KSPSetInitialGuessKnoll(ksp,PETSC_TRUE);
   else
