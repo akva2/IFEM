15d965bec109ef02e2fe0966be2471ae4849fa9a
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2018-11-21 15:50:49 +0100

    s

---
+++ src/ASM/Test/TestASMs1D.C
@@ -0,0 +1,90 @@
+//==============================================================================
+//!
+//! \file TestASMs1D.C
+//!
+//! \date Nov 9 2018
+//!
+//! \author Arne Morten Kvarving / SINTEF
+//!
+//! \brief Tests for structured 1D spline FE models.
+//!
+//==============================================================================
+
+
+#include "GoTools/geometry/ObjectHeader.h"
+#include "GoTools/geometry/SplineCurve.h"
+#include "gtest/gtest.h"
+
+#include <fstream>
+#include <numeric>
+
+
+TEST(TestASMs1D, ExtractBasis)
+{
+  std::ifstream iff("crv.g2");
+  Go::ObjectHeader head;
+  Go::SplineCurve crv;
+  iff >> head >> crv;
+
+  auto&& value = [](double x)
+    {
+      return 0.5*pow(x,3.0)/(pow(1.0-x,3.0)+0.5*pow(x,3.0));
+    };
+
+  auto&& derivative = [](double x)
+      { 
+        return 6.0*pow(1-x,2.0)*pow(x,2.0)/pow(-pow(x,3.0)+6.0*pow(x,2)-6*x+2,2.0);
+      };
+
+  auto&& dderivative = [](double x)
+      { 
+        return -x*(-12.0*pow(x,5)+36*pow(x,4)-24*pow(x,3)-48*pow(x,2)+72*x-24)/pow(-pow(x,3.0)+6*pow(x,2)-6*x+2,3.0);
+      };
+
+  auto&& ddderivative=[](double x)
+      {
+//        return (36.0*pow(x,8.0) - 144.0*pow(x,7.0) + 120.0*pow(x,6.0) + 576.0*pow(x,5)-1872.0*pow(x,4.0)+2112.0*pow(x,3.0)-864.0*pow(x,2.0) + 48.0)/pow(-pow(x,3.0)+6*pow(x,2.0)-6*x+2,4.0);
+        return 12.0*(3.0*pow(x,8.0) - 12.0*pow(x,7.0) + 10.0*pow(x,6.0) + 48.0*pow(x,5)-156.0*pow(x,4.0)+176.0*pow(x,3.0)-72.0*pow(x,2.0) + 4.0)/pow(pow(x,3.0)-6*pow(x,2.0)+6*x-2,4.0);
+      };
+
+    for (auto it = crv.coefs_begin(); it != crv.coefs_end(); ++it)
+      std::cout << *it << " ";
+    std::cout << std::endl;
+
+    for (auto it = crv.rcoefs_begin(); it != crv.rcoefs_end(); ++it)
+      std::cout << *it << " ";
+    std::cout << std::endl;
+
+  std::vector<double> N, dNdu, d2Ndu2, d3Ndu3;
+  for (double x : {0.1, 0.9}) {
+//  for (double x : {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9}) {
+    crv.computeBasis(x, N, dNdu, d2Ndu2, d3Ndu3);
+
+    std::cout << "Value is: " << value(x) << std::endl;
+    std::cout << "Derivative is: " << derivative(x) << std::endl;
+    std::cout << "DDerivative is: " << dderivative(x) << std::endl;
+    std::cout << "DDDerivative is: " << ddderivative(x) << std::endl;
+    std::vector<Go::Point> pt(4);
+    crv.point(pt, x, 3);
+    std::cout << "Basis functions are: ";
+    for (auto& it : N)
+      std::cout << it << " ";
+    std::cout << std::endl;
+    std::cout << "Basis derivatives are: ";
+    for (auto& it : dNdu)
+      std::cout << it << " ";
+    std::cout << std::endl;
+    std::cout << "Basis second derivatives are: ";
+    for (auto& it : d2Ndu2)
+      std::cout << it << " ";
+    std::cout << std::endl;
+    std::cout << "Basis third derivatives are: ";
+    for (auto& it : d3Ndu3)
+      std::cout << it << " ";
+    std::cout << std::endl;
+    std::cout << "Curve 0der value: " << pt[0] << std::endl;
+    std::cout << "Curve 1der value: " << pt[1] << std::endl;
+    std::cout << "Curve 2der value: " << pt[2] << std::endl;
+    std::cout << "Curve 3der value: " << pt[3] << std::endl;
+  }
+}
--- src/ASM/Test/TestASMs2D.C
+++ src/ASM/Test/TestASMs2D.C
@@ -13,7 +13,10 @@
 #include "ASMs2D.h"
 #include "SIM2D.h"
 
+#include "GoTools/geometry/ObjectHeader.h"
+#include "GoTools/geometry/SplineSurface.h"
 #include "gtest/gtest.h"
+#include <fstream>
 #include <numeric>
 
 
@@ -35,3 +38,98 @@ TEST_P(TestASMs2D, Connect)
 
 const std::vector<int> orientations2D = {0,1};
 INSTANTIATE_TEST_CASE_P(TestASMs2D, TestASMs2D, testing::ValuesIn(orientations2D));
+
+
+TEST(TestASMs2D, ExtractBasis)
+{
+  std::ifstream iff("srf.g2");
+  Go::ObjectHeader head;
+  Go::SplineSurface srf;
+  iff >> head >> srf;
+
+  auto&& value = [](double x, double y)
+    {
+      return pow(x,3.0)*pow(y-1,3.0)/(2*((pow(x,3.0)*pow(y-1.0,3.0)/2 - pow(x-1,3.0)*pow(y-1,3.0))));
+    };
+
+  auto&& derx = [](double x, double y)
+    {
+      return 6*pow(x,2.0)*pow(x-1,2.0)/pow(pow(x,3.0)-6*pow(x,2.0)+6*x-2,2.0);
+    };
+
+  auto&& derxx = [](double x, double y)
+    {
+      return -(12*x*(pow(x,5.0)-3*pow(x,4.0)+2*pow(x,3.0)+4*pow(x,2)-6*x+2))/pow(pow(x,3.0)-6*pow(x,2.0)+6*x-2,3.0);
+    };
+
+  auto&& derxxx = [](double x, double y)
+    {
+      return 12.0*(3.0*pow(x,8.0) - 12.0*pow(x,7.0) + 10.0*pow(x,6.0) + 48.0*pow(x,5)-156.0*pow(x,4.0)+176.0*pow(x,3.0)-72.0*pow(x,2.0) + 4.0)/pow(pow(x,3.0)-6*pow(x,2.0)+6*x-2,4.0);
+    };
+
+  auto&& trunc = [](double x)
+  {
+    return fabs(x) < 1e-11 ? 0.0 : x;
+  };
+
+  for (double x : {0.1, 0.9}) {
+    Go::BasisDerivsSf3 derivs;
+    srf.computeBasis(x, x, derivs);
+
+    std::cout << "Value is: " << value(x,x) << std::endl;
+    std::cout << "Derivative is: " << derx(x,x) << std::endl;
+    std::cout << "DDerivative is: " << derxx(x,x) << std::endl;
+    std::cout << "DDDerivative is: " << derxxx(x,x) << std::endl;
+    std::vector<Go::Point> pt(10);
+    srf.point(pt, x, x, 3);
+    std::cout << "N: ";
+    for (auto& it : derivs.basisValues)
+      std::cout << trunc(it) << " ";
+    std::cout << std::endl;
+    std::cout << "dN/du: ";
+    for (auto& it : derivs.basisDerivs_u)
+      std::cout << trunc(it) << " ";
+    std::cout << std::endl;
+    std::cout << "dN/dv: ";
+    for (auto& it : derivs.basisDerivs_v)
+      std::cout << trunc(it) << " ";
+    std::cout << std::endl;
+    std::cout << "d2N/du2: ";
+    for (auto& it : derivs.basisDerivs_uu)
+      std::cout << trunc(it) << " ";
+    std::cout << std::endl;
+    std::cout << "d2N/dv2: ";
+    for (auto& it : derivs.basisDerivs_vv)
+      std::cout << trunc(it) << " ";
+    std::cout << std::endl;
+    std::cout << "d2N/dudv: ";
+    for (auto& it : derivs.basisDerivs_uv)
+      std::cout << trunc(it) << " ";
+    std::cout << std::endl;
+    std::cout << "d3N/du3: ";
+    for (auto& it : derivs.basisDerivs_uuu)
+      std::cout << trunc(it) << " ";
+    std::cout << std::endl;
+    std::cout << "d3N/dv3: ";
+    for (auto& it : derivs.basisDerivs_vvv)
+      std::cout << trunc(it) << " ";
+    std::cout << std::endl;
+    std::cout << "d3N/du2dv: ";
+    for (auto& it : derivs.basisDerivs_uuv)
+      std::cout << trunc(it) << " ";
+    std::cout << std::endl;
+    std::cout << "d3N/dudv2: ";
+    for (auto& it : derivs.basisDerivs_uvv)
+      std::cout << trunc(it) << " ";
+    std::cout << std::endl;
+    const std::array<std::string,10> lab = {"value", "d/du", "d/dv", "d2/du2", "d2/dudv",
+                                            "d2/dv2", "d3/du3", "d3/du2dv", 
+                                             "d3/dudv2", "d3/dv3"};
+    auto it = lab.begin();
+    for (auto& p : pt) {
+      std::cout << "Surface " << *it << ": " 
+                << trunc(p[0]) << " " << trunc(p[1]) << std::endl;
+      ++it;
+    }
+  }
+}
