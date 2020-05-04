a259f199e6261cd560644d469a30632742f6c6fe
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2020-05-04 10:14:46 +0200

    add sanity checking for thread groups in ASMu2Dmx

--- src/ASM/LR/ASMu2Dmx.C
+++ src/ASM/LR/ASMu2Dmx.C
@@ -1084,6 +1084,13 @@ void ASMu2Dmx::generateThreadGroups (const Integrand& integrand, bool silence,
 
   LR::generateThreadGroups(threadGroups,threadBasis,
                            ASMmxBase::Type == ASMmxBase::SUBGRID ? this->getBasis(2) : nullptr);
+
+  std::vector<const LR::LRSpline*> bases;
+  for (const std::shared_ptr<LR::LRSplineSurface>& basis : m_basis)
+    bases.push_back(basis.get());
+
+  this->checkThreadGroups(threadGroups[0], bases, threadBasis);
+
   if (silence || threadGroups[0].size() < 2) return;
 
   std::cout <<"\nMultiple threads are utilized during element assembly.";
