6d72e989b55a0b4615b6a65833616324c6d41810
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2023-08-31 14:10:39 +0200

    ASMu2Dmx: use original spline for geometry with SUBGRID

--- src/ASM/LR/ASMu2Dmx.C
+++ src/ASM/LR/ASMu2Dmx.C
@@ -223,10 +223,13 @@ bool ASMu2Dmx::generateFEMTopology ()
         projB = m_basis[2-ASMmxBase::itgBasis];
     }
 
-    if (ASMmxBase::Type == ASMmxBase::SUBGRID)
+    if (ASMmxBase::Type == ASMmxBase::SUBGRID) {
       projB2 = refB = createLR(*otherBasis);
-    else
+      geomB = m_basis[1];
+    } else {
       refB = projB;
+      geomB = m_basis[itgBasis-1];
+    }
 
     is_rational = tensorspline->rational();
     delete tensorspline;
@@ -284,7 +287,6 @@ bool ASMu2Dmx::generateFEMTopology ()
   std::cout <<"NEL = "<< nel <<" NNOD = "<< nnod << std::endl;
 #endif
 
-  geomB = m_basis[itgBasis-1];
   this->generateBezierBasis();
   this->generateBezierExtraction();
 
@@ -1223,7 +1225,6 @@ void ASMu2Dmx::swapProjectionBasis ()
     std::swap(projB, projB2);
     std::swap(projThreadGroups, proj2ThreadGroups);
     lrspline = m_basis[ASMmxBase::itgBasis-1];
-    geomB = lrspline;
     this->generateBezierBasis();
     this->generateBezierExtraction();
   }
