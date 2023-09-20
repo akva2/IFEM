9b54f553201b22f30d4d5707517ab47abe9ab853
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2023-09-20 23:37:30 +0200

    added: RealFunc::gradient
    returns the gradient of the function as a Vec3

--- src/Utility/Test/TestFunctions.C
+++ src/Utility/Test/TestFunctions.C
@@ -46,6 +46,66 @@ TEST(TestScalarFunc, ParseDerivative)
 }
 
 
+TEST(TestRealFunc, GradientFD)
+{
+  const char* f1 = "sin(x)*sin(y)*sin(z)";
+
+  const double eps = 1e-6;
+
+  EvalFunction f(f1, eps);
+
+  EXPECT_TRUE(f.isConstant());
+
+  for (double x : {0.1, 0.2, 0.3})
+    for (double y : {0.5, 0.6, 0.7})
+      for (double z : {0.8, 0.9, 1.0}) {
+        const Vec3 X(x,y,z);
+        const Vec3 r((sin(x+0.5*eps)-sin(x-0.5*eps))*sin(y)*sin(z) / eps,
+                     sin(x)*(sin(y+0.5*eps)-sin(y-0.5*eps))*sin(z) / eps,
+                     sin(x)*sin(y)*(sin(z+0.5*eps)-sin(z-0.5*eps)) / eps);
+        EXPECT_NEAR(f.deriv(X, 1), r[0], 1e-8);
+        EXPECT_NEAR(f.deriv(X, 2), r[1], 1e-8);
+        EXPECT_NEAR(f.deriv(X, 3), r[2], 1e-8);
+        const Vec3 grad = f.gradient(X);
+        EXPECT_NEAR(grad[0], r[0], 1e-8);
+        EXPECT_NEAR(grad[1], r[1], 1e-8);
+        EXPECT_NEAR(grad[2], r[2], 1e-8);
+      }
+}
+
+
+TEST(TestRealFunc, Gradient)
+{
+  const char* f1 = "sin(x)*sin(y)*sin(z)";
+  const char* d1 = "cos(x)*sin(y)*sin(z)";
+  const char* d2 = "sin(x)*cos(y)*sin(z)";
+  const char* d3 = "sin(x)*sin(y)*cos(z)";
+
+  EvalFunction f(f1);
+  f.addDerivative(d1, "", 1);
+  f.addDerivative(d2, "", 2);
+  f.addDerivative(d3, "", 3);
+
+  EXPECT_TRUE(f.isConstant());
+
+  for (double x : {0.1, 0.2, 0.3})
+    for (double y : {0.5, 0.6, 0.7})
+      for (double z : {0.8, 0.9, 1.0}) {
+        const Vec3 X(x,y,z);
+        const Vec3 r(cos(x)*sin(y)*sin(z),
+                     sin(x)*cos(y)*sin(z),
+                     sin(x)*sin(y)*cos(z));
+        EXPECT_DOUBLE_EQ(f.deriv(X, 1), r[0]);
+        EXPECT_DOUBLE_EQ(f.deriv(X, 2), r[1]);
+        EXPECT_DOUBLE_EQ(f.deriv(X, 3), r[2]);
+        const Vec3 grad = f.gradient(X);
+        EXPECT_DOUBLE_EQ(grad[0], r[0]);
+        EXPECT_DOUBLE_EQ(grad[1], r[1]);
+        EXPECT_DOUBLE_EQ(grad[2], r[2]);
+      }
+}
+
+
 TEST(TestEvalFunction, ExtraParam)
 {
   EvalFunction f("x*foo");
