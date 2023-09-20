0aee935c081bd77112fead4d9af33a7a19424bd7
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2023-09-20 23:37:30 +0200

    added: RealFunc::gradient
    returns the gradient of the function as a Vec3

--- src/Utility/ExprFunctions.C
+++ src/Utility/ExprFunctions.C
@@ -329,8 +329,8 @@ void EvalFunction::addDerivative (const std::string& function,
 {
   if (d1 > 0 && d1 <= 3 && d2 < 1) // A first derivative is specified
   {
-    if (!gradient[--d1])
-      gradient[d1] = std::make_unique<EvalFunction>((variables+function).c_str());
+    if (!fgradient[--d1])
+      fgradient[d1] = std::make_unique<EvalFunction>((variables+function).c_str());
   }
   else if ((d1 = voigtIdx(d1,d2)) >= 0) // A second derivative is specified
   {
@@ -372,8 +372,8 @@ Real EvalFunction::deriv (const Vec3& X, int dir) const
     return Real(0);
   else if (dir < 4)
   {
-    if (gradient[--dir])
-      return gradient[dir]->evaluate(X);
+    if (fgradient[--dir])
+      return fgradient[dir]->evaluate(X);
 
     // Evaluate spatial derivative using central difference
     Vec4 X0, X1;
