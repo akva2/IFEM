1951dead18ed9c0d9ac4ebeb57e3f41ad0a2f6ce
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2023-09-20 22:51:13 +0200

    changed: use a common implementation for setNoDims()
    add a helper template and specialize this
    for the types instead. preparation for upcoming changes
    to workaround c++'s inability to partially specialize
    member functions

--- src/Utility/ExprFunctions.h
+++ src/Utility/ExprFunctions.h
@@ -208,9 +208,12 @@ public:
       func->setParam(name, value);
   }
 
+  //! \brief Returns number of spatial dimension.
+  size_t getNoSpaceDim() const { return nsd; }
+
 protected:
   //! \brief Sets the number of spatial dimensions (default implementation).
-  void setNoDims() { ParentFunc::ncmp = nsd = p.size(); }
+  void setNoDims();
 
   //! \brief Evaluates the function expressions.
   Ret evaluate(const Vec3& X) const override;
@@ -222,11 +225,5 @@ using VecFuncExpr = EvalMultiFunction<VecFunc,Vec3>;
 using TensorFuncExpr = EvalMultiFunction<TensorFunc,Tensor>;
 //! Symmetric tensor-valued function expression
 using STensorFuncExpr = EvalMultiFunction<STensorFunc,SymmTensor>;
-
-//! \brief Specialization for tensor functions.
-template<> void TensorFuncExpr::setNoDims();
-
-//! \brief Specialization for symmetric tensor functions.
-template<> void STensorFuncExpr::setNoDims();
 
 #endif
