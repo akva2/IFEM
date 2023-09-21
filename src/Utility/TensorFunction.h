5d6861578f79d19ade56701f78047d6fa24cc008
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2023-09-21 12:26:52 +0200

    added: (Tensor|Vec)Func::Gradient
    returns the gradient of the function

--- src/Utility/TensorFunction.h
+++ src/Utility/TensorFunction.h
@@ -15,6 +15,7 @@
 #define _TENSOR_FUNCTION_H
 
 #include "Function.h"
+#include "matrixnd.h"
 #include "Tensor.h"
 
 
@@ -49,6 +50,23 @@ public:
   {
     return this->evaluate(X).trace();
   }
+
+  //! \brief Evaluates first derivatives of the function.
+  utl::matrix3d<Real> gradient(const Vec3& X) const
+  {
+    const size_t nsd = sqrt(this->dim());
+    utl::matrix3d<Real> result(nsd, nsd, nsd);
+    result.fill(this->evalGradient(X).data());
+
+    return result;
+  }
+
+protected:
+  //! \brief Returns the gradient of the function as a 1D array.
+  virtual std::vector<Real> evalGradient(const Vec3& X) const
+  {
+    return {};
+  }
 };
 
 
@@ -59,6 +77,20 @@ public:
 class STensorFunc : public utl::SpatialFunction<SymmTensor>,
                     public FunctionBase
 {
+  //! \brief Returns the flat indices of the symmetric tensor.
+  size_t index(size_t nsd, size_t i, size_t j) const
+  {
+    if (i == j)
+      return i-1; // diagonal term
+    else if (nsd == 2)
+      return ncmp-1; // off-diagonal term (2D)
+
+    if (i == j+1 || i+2 == j)
+      std::swap(i,j);
+
+    return i+2; // upper triangular term (3D)
+  }
+
 protected:
   //! \brief The constructor is protected to allow sub-class instances only.
   STensorFunc(size_t n = 0, bool with33 = false)
@@ -84,6 +116,28 @@ public:
   virtual Real getScalarValue(const Vec3& X) const
   {
     return this->evaluate(X).trace();
+  }
+
+  //! \brief Evaluates first derivatives of the function.
+  utl::matrix3d<Real> gradient(const Vec3& X) const
+  {
+    const size_t nsd = ncmp > 5 ? 3 : (ncmp > 2 ? 2 : 1);
+    utl::matrix3d<Real> result(nsd,nsd,nsd);
+    const std::vector<Real> temp = this->evalGradient(X);
+
+    for (size_t d = 1; d <= nsd; ++d)
+      for (size_t i = 1; i <= nsd; ++i)
+        for (size_t j = 1; j <= nsd; ++j)
+          result(i,j,d) = temp[index(nsd,i,j) + (d-1)*ncmp];
+
+    return result;
+  }
+
+protected:
+  //! \brief Returns the gradient of the function as a 1D array.
+  virtual std::vector<Real> evalGradient(const Vec3& X) const
+  {
+    return {};
   }
 };
 
