f1b1f6754b7120036eae2ce5c22f11a08d0344ff
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2023-09-23 18:13:09 +0200

    changed: rename EvalFunc::derivative to EvalFunc::addDerivative
    consistent with EvalFunction and more descriptive of what it does

--- src/Utility/ExprFunctions.C
+++ src/Utility/ExprFunctions.C
@@ -233,7 +233,7 @@ EvalFunc::EvalFunc (const char* function, const char* x, Real eps)
 EvalFunc::~EvalFunc () = default;
 
 
-void EvalFunc::derivative (const std::string& function, const char* x)
+void EvalFunc::addDerivative (const std::string& function, const char* x)
 {
   if (!gradient)
     gradient = std::make_unique<EvalFunc>(function.c_str(),x);
